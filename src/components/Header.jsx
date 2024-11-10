bb73250ca2ed2927a435dd2eef6885c63788b99c
anirudhece2020 <anirudhece2020@gmail.com>
2024-11-10 18:47:17 +0000

    till 28:00/ have to debug header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
+import { SignIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
+
 const Header = () => {
     return (
         <div className="flex justify-between items-center bg-blue-800 p-2">
@@ -9,7 +11,19 @@ const Header = () => {
                 <Button className='mx-1' >
                     <Link href={'/create'}>Create event</Link>
                 </Button>
-                <Button className='mx-1'>Login</Button>
+
+                <SignedOut>{/* this will only render when user is not loged in */}
+                    <SignInButton forceRedirectUrl='/dashboard'> 
+                        <Button className='mx-1'>Login</Button>
+                    </SignInButton>
+                </SignedOut>
+
+                <SignIn> {/* this will only render when user is loged in */}
+                    <UserButton />
+                    <Button className='mx-1'>
+                        <Link href={'/sign-up'}>Sign up</Link>
+                    </Button>
+                </SignIn>
             </div>
         </div>
     )
