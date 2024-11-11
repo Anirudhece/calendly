dbf1b8894f307593d50ef3dd46f09075c45df313
anirudhece2020 <anirudhece2020@gmail.com>
2024-11-11 07:20:38 +0000

    authentication changes in clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
-import { SignIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
 
 const Header = () => {
     return (
         <div className="flex justify-between items-center bg-blue-800 p-2">
             <Link href={'/'}>Scheduler</Link>
-            <div>
-                <Button className='mx-1' >
+            <div className='flex space-x-3'>
+                <Button >
                     <Link href={'/create'}>Create event</Link>
                 </Button>
 
                 <SignedOut>{/* this will only render when user is not loged in */}
-                    <SignInButton forceRedirectUrl='/dashboard'> 
-                        <Button className='mx-1'>Login</Button>
+                    <SignInButton forceRedirectUrl='/dashboard'>
+                        <Button>Login</Button>
                     </SignInButton>
                 </SignedOut>
 
-                <SignIn> {/* this will only render when user is loged in */}
+                <SignedIn >{/* this will only render when user is loged in */}
                     <UserButton />
-                    <Button className='mx-1'>
-                        <Link href={'/sign-up'}>Sign up</Link>
-                    </Button>
-                </SignIn>
+                </SignedIn>
             </div>
         </div>
     )
--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/events(.*)', 'meetings(.*)', '/availabilty(.*)'])
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/events(.*)', 'meetings(.*)', '/availability(.*)'])
 
-export default clerkMiddleware((auth, req) => {
-  if (!auth().userId && isProtectedRoute(req)) {
-    return auth().redirectToSignIn();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn()
   }
 });
 
