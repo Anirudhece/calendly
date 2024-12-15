83a4ea106ec3247e0459ad3a99487517eacde25a
anirudhece2020 <anirudhece2020@gmail.com>
2024-12-15 10:27:01 +0000

    navigation branch push

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
+
+const appLayout = ({ children }) => {
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </>
+  );
+};
+
+export default appLayout;
---
+++ src/app/(main)/meetings/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      pagepage page pagepage page pagepage page pagepage page pagepage page page
+    </div>
+  );
+};
+
+export default page;
